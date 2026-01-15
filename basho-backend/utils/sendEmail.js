919312694fcacfaa73c14de4731d632e6adefbf4
Manasa Mohan <mohanmanasa05@gmail.com>
2026-01-15 21:16:33 +0530

    Fix send-otp to respond immediately

--- basho-backend/utils/sendEmail.js
+++ basho-backend/utils/sendEmail.js
@@ -2,26 +2,21 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, text }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // TLS
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
 
-  const mailPromise = transporter.sendMail({
+  return transporter.sendMail({
     from: `"BASHO" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     text,
   });
-
-  // timeout safety
-  const timeoutPromise = new Promise((_, reject) =>
-    setTimeout(() => reject(new Error("Email timeout")), 15000)
-  );
-
-  return Promise.race([mailPromise, timeoutPromise]);
 };
 
 module.exports = sendEmail;
